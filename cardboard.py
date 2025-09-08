b0e80dfba72886f6eceae04f66aed1e08f8fc9b2
Bert Huijben <bert@qqmail.nl>
2025-09-08 09:51:09 +0200

    First steps towards python package
    Backport regression tests
    
    Migrate to python package
    
    Switch to package layout
    
    Fix formatting, etc
    
    Rename boxmaker scripts. Use ampscm domain for uniqueness

---
+++ cardboard.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python -t
+'''
+Generates Inkscape SVG file containing box components needed to
+CNC (laser/mill) cut a card board box
+
+Original Tabbed Box Maker Copyright (C) 2011 elliot white
+Cardboard Box Maker Copyright (C) 2024 Brad Goodman
+
+Changelog:
+14/05/2024 Brad Goodman:
+    - Created from Tabbed Box Maker
+
+ This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from tabbedboxmaker.cardboard import main
+
+if __name__ == "__main__":
+    main(cli=True)
